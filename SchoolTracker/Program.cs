564eb2be0365070d08735cc205a8a3390fafdfaa
olaruVlad <vlad.olaru@theaccessgroup.com>
2023-06-22 14:05:39 +0300

    error

--- SchoolTracker/Program.cs
+++ SchoolTracker/Program.cs
@@ -11,7 +11,7 @@ namespace SchoolTracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("students in your class?");
+            Console.WriteLine("Make commit number two on Main branch");
             var studentCount = int.Parse(Console.ReadLine());
 
             var studentNames = new string[studentCount];
